c24e4c30e645384517279b445527ef25e6b02ab5
Msushi <39556839+Msushi@users.noreply.github.com>
2024-04-15 16:38:12 -0400

    First commit (real)

---
+++ frontend/src/components/Header/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h2>Realtime Chat App</h2>
+        </div>
+    )
+    
+}
+
+export default Header;
